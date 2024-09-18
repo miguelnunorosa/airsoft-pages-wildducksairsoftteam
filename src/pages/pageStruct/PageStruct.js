364a9e0520ddeaa967f9e900f4c97c38a2ff5819
Miguel <miguelrosa.programacao@gmail.com>
2024-09-18 15:55:43 +0000

    first commit

---
+++ src/pages/pageStruct/PageStruct.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { HashRouter, Route } from "react-router-dom";
+
+import "./pageStruct.css";
+
+
+
+
+
+class PageStruct extends Component {
+   render(){
+    return(
+        <div>
+            <h3>Hello WORLD!!</h3>
+        </div>
+    );
+   }
+}
+
+export default PageStruct;
